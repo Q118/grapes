958204d4e0faca05520a6233323908a951fa8d73
Q118 <shelbyfish91@gmail.com>
2020-09-25 13:54:50 -0700

    JSON data on page load propogated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import MainContainer from './components/MainContainer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./css/app.css";
@@ -15,6 +15,18 @@ function App() {
   //state to store the selected activity box
   const [ activities, setActivities ] = useState(sampleActivities);
   const [ selectedActivityId, setSelectedActivityId] = useState(sampleActivities);
+
+  //for each activity, we just want to compare the activity ID to the ID of our selected recipe
+	const selectedActivity = activities.find(
+		(activity) => activity.id === selectedActivityId
+  );
+  
+  //on load
+  useEffect(() => {
+		const activityJSON = localStorage.getItem(LOCAL_STORAGE_KEY);
+		//check if it exists yet
+		if (activityJSON != null) setActivities(JSON.parse(activityJSON));
+	}, []);
 
   return (
     <>
