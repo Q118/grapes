f2fa10ad98cb2ad59872aefcdf6cccf633cbaef9
Q118 <shelbyfish91@gmail.com>
2020-09-25 13:02:37 -0700

    component setup for activity square

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import MainContainer from './components/MainContainer';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import "./css/app.css";
 
 function App() {
   return (
--- src/components/ActivityTable.js
+++ src/components/ActivityTable.js
@@ -1,23 +1,45 @@
 /** @format */
 
 import React from "react";
+import ActivtySquare from './ActivitySquare';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col'
+import ActivitySquare from "./ActivitySquare";
+
+// each box should have a text area to be filled
 
 export default function ActivityTable() {
 	return (
 		<div>
 			<Container className="activity--table__container">
 				<Row>
-                    <Col>1 of 3</Col>
-					<Col>2 of 3</Col>
-					<Col>3 of 3</Col>
+                    <Col className="box box--g">
+                        <h3>G</h3>
+                        <ActivitySquare />
+                    </Col>
+					<Col className="box box--r">
+                        <h3>R</h3> 
+                        <ActivitySquare />
+                    </Col>
+					<Col className="box box--a">
+                        <h3>A</h3>
+                        <ActivitySquare />
+                    </Col>
 				</Row>
 				<Row>
-					<Col>1 of 3</Col>
-					<Col>2 of 3</Col>
-					<Col>3 of 3</Col>
+					<Col className="box box--p">
+                    <h3>P</h3>
+                        <ActivitySquare />
+                    </Col>
+					<Col className="box box--e">
+                    <h3>E</h3>
+                        <ActivitySquare />
+                    </Col>
+					<Col className="box box--s">
+                    <h3>S</h3>
+                        <ActivitySquare />
+                    </Col>
 				</Row>
 			</Container>
 		</div>
