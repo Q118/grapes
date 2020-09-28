87e3416a69f4448f99ef5287eec8b2b54c24b813
Q118 <shelbyfish91@gmail.com>
2020-09-28 11:02:16 -0700

    set up environment to hold activity data

--- src/components/ActivitySquare.js
+++ src/components/ActivitySquare.js
@@ -1,22 +1,45 @@
+/** @format */
+
 import React, { useContext } from "react";
 import { ActivityContext } from "../App";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
+
 
 export default function ActivitySquare(props) {
-    //set up the initial state to hold name and activity
+	//set up the initial state to hold name and activity
+	const { handleRecipeSelect } = useContext(ActivityContext);
 
-    const { meanings } = useContext(ActivityContext);
+	const { id, name, meaning, start } = useContext(ActivityContext);
 
-    return (
-        <>
-            <textarea
-                type="activity"
-                name="activity"
-                className="activity__input"
-                placeholder= {meanings[0]} 
-            />
-        </>
-    )
+	// **To do* add in suggestions into props//
+
+	return (
+		<div className="activity__square">
+			<div className="activity__header">
+				<div className="activity__title">
+					<h3>{name}</h3>
+				</div>
+				<div className="edit--button__container">
+					<button
+						className="btn btn--primary mr-1"
+						onClick={() => handleRecipeSelect(id)}>
+						<FontAwesomeIcon icon={faEdit} />
+					</button>
+				</div>
+    <div className="activity__meaning"><h4>{meaning}</h4></div>       
+            </div>
+            <div className="activity__row"></div>
+
+			<textarea
+				type="activity"
+				name="activity"
+				className="activity__input"
+				placeholder={meaning}
+			/>
+		</div>
+	);
 }
 
 // corelated with proper selectedID
