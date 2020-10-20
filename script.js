45cde4f81237daaae6b6fb325d025438051a44ca
Q118 <shelbyfish91@gmail.com>
2020-10-20 11:31:20 -0700

    displaying only one container on page load

---
+++ script.js
@@ -0,0 +1,9 @@
+// variables
+const welcomeContainerOne = document.querySelector(".welcomeOne");
+const welcomeContainerTwo = document.querySelector(".welcomeTwo");
+
+//elements to be hidden on page load 
+window.addEventListener("load", function(e) {
+    welcomeContainerTwo.style.visibility = "hidden";
+    // TODO: add elements here as i create them 
+})
