3d63c0c0afb14653b9b1969614765766f112c6bc
Q118 <shelbyfish91@gmail.com>
2020-10-20 11:37:26 -0700

    event deligation tobegin button

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 // variables
-const welcomeContainerOne = document.querySelector(".welcomeOne");
-const welcomeContainerTwo = document.querySelector(".welcomeTwo");
+//containers
+const welcomeContainerOne = document.querySelector('.welcomeOne');
+const welcomeContainerTwo = document.querySelector('.welcomeTwo');
+
+//buttons
+const beginBtn = document.querySelector('.beginBtn');
+const startBtn = document.querySelector('.startBtn');
 
 //elements to be hidden on page load 
-window.addEventListener("load", function(e) {
+window.addEventListener("load",  () => {
     welcomeContainerTwo.style.visibility = "hidden";
     // TODO: add elements here as i create them 
-})
+});
+
+
+
+
+
+
+//event listeners for buttons
+beginBtn.addEventListener("click", () => {
+    welcomeContainerTwo.style.visibility = "visible";
+    welcomeContainerOne.style.visibility = "hidden"
+});
