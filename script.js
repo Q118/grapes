0b02d18fcd675b8279d85eec7bb16c36cdba41d3
Q118 <shelbyfish91@gmail.com>
2020-10-20 12:05:53 -0700

    text content replaced on nest page

--- script.js
+++ script.js
@@ -2,17 +2,22 @@
 //containers
 const welcomeContainerOne = document.querySelector('.welcomeOne');
 const welcomeContainerTwo = document.querySelector('.welcomeTwo');
-const instructionsContainer = document.querySelector('.instructionsContainer')
+const mainContainer = document.querySelector('.mainContainer');
+const titleContainer = document.querySelector('.titleContainer');
+const subtitleContainer = document.querySelector('.subtitleContainer');
+const startingContainer = document.querySelector('.startingContainer');
+const userContainer = document.querySelector('.userContainer');
+
 
 //buttons
 const beginBtn = document.querySelector('.beginBtn');
 const startBtn = document.querySelector('.startBtn');
 
-//elements to be hidden on page load 
-window.addEventListener("load",  () => {
-    welcomeContainerTwo.style.visibility = "hidden";
-    // TODO: add elements here as i create them 
-});
+// //elements to be hidden on page load 
+// window.addEventListener("load",  () => {
+//     //welcomeContainerTwo.style.visibility = "hidden";
+//     // TODO: add elements here as i create them 
+// });
 
 
 
@@ -21,7 +26,9 @@ window.addEventListener("load",  () => {
 
 //event listeners for buttons
 beginBtn.addEventListener("click", () => {
-    welcomeContainerTwo.style.visibility = "visible";
-    welcomeContainerOne.style.visibility = "hidden";
-    welcomeContainerTwo.style.transform = "translate(0px, -500px)"
+   startingContainer.textContent = `How this app works`;
+   userContainer.textContent = `it works like um dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur.Lorem ipsu`;
+   
 });
+
+startBtn
