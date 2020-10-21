c2df6c3a46b66036c573cdbc4724f1d612e6f074
Q118 <shelbyfish91@gmail.com>
2020-10-21 15:56:19 -0700

    rendering data to each letter from local storage

--- assets/chart.js
+++ assets/chart.js
@@ -2,17 +2,32 @@
 
 //variables
 const dateContainer = document.querySelector(".dateContainer");
+const userG = document.querySelector(".userG");
+const userR = document.querySelector(".userR");
+const userA = document.querySelector(".userA");
+const userP = document.querySelector(".userP");
+const userE = document.querySelector(".userE");
+const userS = document.querySelector(".userS");
 
 const newDate  = new Date();
 const today = (newDate.getMonth()+1)+'-'+newDate.getDate()+'-'+newDate.getFullYear();
 
 //! get proper input from   storage
-const storedInput = localStorage.getItem(1);
-
+const storedG = localStorage.getItem(1);
+const storedR = localStorage.getItem(2);
+const storedA = localStorage.getItem(3);
+const storedP = localStorage.getItem(4);
+const storedE = localStorage.getItem(5);
+const storedS = localStorage.getItem(6);
 
 window.addEventListener("load", () => {
     dateContainer.innerHTML = today;
-    console.log(storedInput);
+    userG.textContent = storedG;
+    userR.textContent = storedR;
+    userA.textContent = storedA;
+    userP.textContent = storedP;
+    userE.textContent = storedE;
+    userS.textContent = storedS;
 });
 
 
